90a6d30a9dabba5163b3be2a643f640ae845a517
Wenjie Cui <wc190@duke.edu>
2023-04-05 14:37:19 -0400

    Fix format of the return of holding function

--- tests/test_portfolio.py
+++ tests/test_portfolio.py
@@ -1,6 +1,12 @@
 import pandas as pd
 import numpy as np
 
+import sys
+sys.path.append('../src/bigbucks_port')
+import portfolio
+
+from bigbucks_db import *
+
 # Example data
 er = np.array([.05, .04, .03])
 sd = np.array([.2, .1, .05])
@@ -9,3 +15,13 @@ corr = [[1,.5,0],
         [0,.5,1]]
 covar = np.diag(sd) @ corr @ np.diag(sd)
 
+# Connect to supabase
+url = "https://lhjpufbcymwhprgzfbwt.supabase.co"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxoanB1ZmJjeW13aHByZ3pmYnd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzk2MDY3MDMsImV4cCI6MTk5NTE4MjcwM30.42A0qtrLYChbrdUzjf1E7TRgHionW5xrZRK-e9wBqPk"
+objs = Table_View(url, key)
+
+# Connect to Alpha Vantage
+STOCK_API_KEYS = "9Q91BWGMOE13WOR3"
+objs_realtime = Buy_And_Sell(STOCK_API_KEYS)
+
+print(portfolio.holding_json(objs,6))
