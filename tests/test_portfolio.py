e1f33a681fb5e13e115d9957a39321dfd0695fcd
Wenjie Cui <wc190@duke.edu>
2023-04-10 23:46:16 -0400

    Fix catching error for no prices

--- tests/test_portfolio.py
+++ tests/test_portfolio.py
@@ -9,18 +9,18 @@ import portfolio
 
 from bigbucks_db import *
 
-# Example data
-er = np.array([.05, .04, .03])
-sd = np.array([.2, .1, .05])
-corr = [[1,.5,0],
-        [.5,1,.5],
-        [0,.5,1]]
-covar = np.diag(sd) @ corr @ np.diag(sd)
+# # Example data
+# er = np.array([.05, .04, .03])
+# sd = np.array([.2, .1, .05])
+# corr = [[1,.5,0],
+#         [.5,1,.5],
+#         [0,.5,1]]
+# covar = np.diag(sd) @ corr @ np.diag(sd)
 
-# means, stds = portfolio.frontier(objs,6,100)
-# plt.scatter(stds, means)
-# plt.show()
-# print(portfolio.frontier_json(objs,None,3))
-# js = portfolio.return_json(objs,6)
-# print(json.loads(js)[0])
-# print(portfolio.spy_json(objs))
+url = ""
+key = ""
+STOCK_API_KEYS = ""
+objs_realtime = Buy_And_Sell(STOCK_API_KEYS)
+objs = Table_View(url, key)
+
+print(portfolio.holding_return(objs,5,objs_realtime))
